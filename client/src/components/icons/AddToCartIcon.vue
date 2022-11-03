6651475a6d2d8640e78a0145572b79d04244fd02
Maccbrainy <iketakumichaelnedum@gmail.com>
2022-11-03 12:44:14 +0100

    Initial commit

---
+++ client/src/components/icons/AddToCartIcon.vue
@@ -0,0 +1,27 @@
+<template>
+  <svg
+    viewBox="0 0 25 24"
+    fill="none"
+    width="24"
+    height="24"
+    xlmns="http://www.w3.org/2000/svg"
+    class="h-6 w-6"
+  >
+    <g>
+      <path
+        d="M12.5 5V19"
+        stroke="currentColor"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      ></path>
+      <path
+        d="M5.5 12H19.5"
+        stroke="currentColor"
+        stroke-width="1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      ></path>
+    </g>
+  </svg>
+</template>
