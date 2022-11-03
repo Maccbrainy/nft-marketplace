533f024d366caa7cf6f9b95758ab1a222d65befc
Maccbrainy <iketakumichaelnedum@gmail.com>
2022-11-03 12:56:13 +0100

    css updates and file rename

--- client/src/pages/TheHomePageView.vue
+++ client/src/pages/TheHomePageView.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
 import Welcome from "../components/WelcomeHero.vue";
-import ShowTable from "../components/ShowTable.vue";
+import TableCollections from "../components/TableCollections.vue";
 </script>
 <template>
   <section
-    class="relative top-10 pb-20 flex flex-col justify-center items-center max-w-screen-2xl px-8"
+    class="relative top-10 pb-20 flex flex-col justify-center items-center max-w-screen-2xl px-4 sm:px-6 md:px-7 lg:px-8 mx-auto"
   >
     <Welcome />
-    <ShowTable />
+    <TableCollections />
   </section>
 </template>
