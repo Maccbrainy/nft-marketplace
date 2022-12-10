34321d1a24abdb7167ece59e021eff8d37a36f90
Maccbrainy <iketakumichaelnedum@gmail.com>
2022-12-10 10:59:10 +0100

    Props definitions

--- client/src/components/buttonui/ButtonInput.vue
+++ client/src/components/buttonui/ButtonInput.vue
@@ -1,15 +1,25 @@
 <script setup lang="ts">
 defineProps({
   inputType: String,
-  placeHoldertext: String
+  placeHoldertext: String,
+  stepValue: [String, Number],
+  hasSymbol: String
 })
 </script>
 <style scoped>
+/* input::-webkit-input-placeholder {
+  color: rgba(255, 255, 255, 0.6);
+} */
 </style>
 <template>
-  <input
-    class="w-full bg-gray-100 dark:text-darkTheme-text-b dark:bg-darkTheme-bg border border-transparent px-3 py-2.5 rounded-xl hover:border-gray-300 outline-none focus:bg-white focus:border-gray-300 hover:dark:border-darkTheme-hover focus:dark:border-darkTheme-border focus:dark:bg-darkTheme"
-    :type="inputType"
-    :placeholder="placeHoldertext"
-  />
+  <div class="relative flex items-center justify-end">
+    <input
+      class="w-full bg-gray-100 dark:text-darkTheme-text-b dark:bg-darkTheme-bg border border-transparent px-3 py-2.5 rounded-xl hover:border-gray-300 outline-none focus:bg-white focus:border-gray-300 hover:dark:border-darkTheme-hover focus:dark:border-darkTheme-border focus:dark:bg-darkTheme"
+      :type="inputType"
+      :step="stepValue"
+      :placeholder="placeHoldertext"
+    />
+    <span class="absolute right-10">{{ hasSymbol }}</span>
+  </div>
+
 </template>
