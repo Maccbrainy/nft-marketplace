e318db0743bdb754826fc1e126c41f9dc4eb5604
Maccbrainy <iketakumichaelnedum@gmail.com>
2022-12-16 12:01:02 +0100

    UI/UX: updated and strong typed casting

--- client/src/components/buttonui/ButtonMiscellenous.vue
+++ client/src/components/buttonui/ButtonMiscellenous.vue
@@ -1,17 +1,30 @@
 <script setup lang="ts">
-import { ref, type PropType } from "vue";
+import { computed, ref, type PropType } from "vue";
 import type { OptionType } from "@/types";
-defineProps({
+const props = defineProps({
   listOfOptions: {
     type: Array as PropType<OptionType[]>,
-    // required: true,
+    required: false,
+    default: Array as PropType<OptionType[]>,
+  },
+  isActiveOption: {
+    type: String,
   },
   hasListContent: {
     type: Boolean,
     required: true,
   },
+  computeListContent: {
+    type: Boolean,
+    required: false,
+  },
 });
 const openDropDownMenu = ref<Boolean>(false);
+const getActiveOption = computed<OptionType[]>(() => {
+  return props.listOfOptions.filter(
+    (option) => option.id === props.isActiveOption
+  );
+});
 </script>
 <script lang="ts">
 export default {
@@ -21,6 +34,8 @@ export default {
 <template>
   <button
     @click="openDropDownMenu = !openDropDownMenu"
+    v-for="(option, index) in getActiveOption"
+    :key="computeListContent ? option.id + index : 0"
     v-bind="$attrs"
     :class="{
       'bg-gray-100 dark:bg-darkTheme-hover dark:text-darkTheme-text-b':
@@ -28,7 +43,19 @@ export default {
     }"
     class="relative h-full outline-none transition-all px-5 transform active:scale-95 whitespace-nowrap z-20"
   >
-    <slot></slot>
+    <div
+      :class="{
+        'justify-between': computeListContent,
+        'justify-center': !computeListContent,
+      }"
+      class="w-full flex flex-row items-center outline-none"
+    >
+      <span v-show="option.icon && computeListContent"
+        ><component :is="option.icon"></component
+      ></span>
+      <span v-show="computeListContent">{{ option.name }}</span>
+      <slot></slot>
+    </div>
     <div
       v-if="openDropDownMenu && hasListContent"
       class="animate-slide-up absolute w-max min-w-[11rem] overflow-y-auto max-w-xl h-auto max-h-80 border rounded-xl shadow-md dark:border-darkTheme-border right-0 mt-5 z-20"
@@ -44,7 +71,7 @@ export default {
             @click="$emit('selectionAction', option)"
             class="text-black dark:text-white hover:bg-gray-100 text-sm font-semibold dark:hover:bg-darkTheme-hover rounded-xl w-full grid grid-flow-col grid-cols-5 mx-auto py-3 px-3 cursor-pointer"
           >
-            <span v-if="option.icon" class="col-span-1"
+            <span v-show="option.icon" class="col-span-1"
               ><component :is="option.icon"></component
             ></span>
             <span
