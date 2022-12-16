a2b66efb8f744f795f609da3edc962d45b57ae8c
Maccbrainy <iketakumichaelnedum@gmail.com>
2022-12-16 11:59:10 +0100

    Core: Event emission handling and props update

--- client/src/components/buttonui/ButtonInput.vue
+++ client/src/components/buttonui/ButtonInput.vue
@@ -3,8 +3,11 @@ defineProps({
   inputType: String,
   placeHoldertext: String,
   stepValue: [String, Number],
-  hasSymbol: String
-})
+  hasSymbol: String,
+  isCreateNftPage: Boolean,
+  inputValue: [String, Number] 
+});
+defineEmits(["update:inputValue", "inputFocused"]);
 </script>
 <style scoped>
 /* input::-webkit-input-placeholder {
@@ -14,12 +17,18 @@ defineProps({
 <template>
   <div class="relative flex items-center justify-end">
     <input
-      class="w-full bg-gray-100 dark:text-darkTheme-text-b dark:bg-darkTheme-bg border border-transparent px-3 py-2.5 rounded-xl hover:border-gray-300 outline-none focus:bg-white focus:border-gray-300 hover:dark:border-darkTheme-hover focus:dark:border-darkTheme-border focus:dark:bg-darkTheme"
+      v-on:focus="$emit('inputFocused', true)"
+      v-on:blur="$emit('inputFocused', false)"
+      :value="inputValue"
+      @input="$emit('update:inputValue', ($event.target as HTMLInputElement).value)"
+      :class="{
+        'border border-transparent hover:border-gray-300': !isCreateNftPage,
+      }"
+      class="w-full dark:text-darkTheme-text-b rounded-xl px-3 py-2.5 outline-none bg-gray-100 dark:bg-darkTheme-bg focus:bg-white focus:border-gray-300 hover:dark:border-darkTheme-hover focus:dark:border-darkTheme-border focus:dark:bg-darkTheme"
       :type="inputType"
       :step="stepValue"
       :placeholder="placeHoldertext"
     />
     <span class="absolute right-10">{{ hasSymbol }}</span>
   </div>
-
 </template>
