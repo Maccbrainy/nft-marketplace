27dda2e8dbcf2fdfd648f8dac0cf6d3af6624701
Maccbrainy <iketakumichaelnedum@gmail.com>
2022-12-16 11:49:34 +0100

    page index updated

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -2,3 +2,4 @@ export { default as TheCollectionPageView } from "@/pages/TheCollectionPageView.
 export { default as TheCreatePageView } from "@/pages/TheCreatePageView.vue"
 export { default as TheHomePageView } from "@/pages/TheHomePageView.vue"
 export { default as TheTokenPageView } from "@/pages/TheTokenPageView.vue"
+export { default as TheBlockchainOptionsAndErcTypesPageView } from "@/pages/TheBlockchainOptionsAndErcTypesPageView.vue"
