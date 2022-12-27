18f99879f9903e76812a88cdc87d612e80b9a0ff
Maccbrainy <iketakumichaelnedum@gmail.com>
2022-12-27 08:56:07 +0100

    Core: App level provide plugin functionality

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,11 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import provider from "./provider";
 import router from "./router";
-
 import "./assets/main.css";
 
 const app = createApp(App);
-
+app.use(provider);
 app.use(router);
-
 app.mount("#app");
