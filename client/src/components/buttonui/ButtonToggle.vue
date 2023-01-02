63d7f93e5002efcf301c1b22a092e6c8f85d92fb
Maccbrainy <iketakumichaelnedum@gmail.com>
2023-01-02 16:03:29 +0100

    UI: dark theme mode

--- client/src/components/buttonui/ButtonToggle.vue
+++ client/src/components/buttonui/ButtonToggle.vue
@@ -12,15 +12,15 @@ const switchChange = ref(props.switchChangeProvide);
     :value="switchChange"
     :aria-checked="switchChange"
     :class="{
-      'bg-gray-200': !switchChange,
-      'bg-black': switchChange,
+      'bg-gray-200 dark:bg-darkTheme-hover ': !switchChange,
+      'bg-black dark:bg-white': switchChange,
     }"
     class="relative min-w-[40px] w-10 h-5 flex items-center rounded-xl overflow-hidden cursor-pointer transition"
   >
     <div
       :class="{
-        'translate-x-[170%] bg-white': switchChange,
-        'bg-black': !switchChange,
+        'translate-x-[170%] bg-white dark:bg-black': switchChange,
+        'bg-black dark:bg-white': !switchChange,
       }"
       class="absolute rounded-full w-3 h-3 m-1 transition-all duration-300"
     ></div>
