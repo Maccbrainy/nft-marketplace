b591ef4c3222005dd2845c59a9c93712dd6a14f5
Maccbrainy <iketakumichaelnedum@gmail.com>
2023-01-14 23:52:00 +0100

    UX/UI: Table image carousel implementation

--- client/src/components/tables/TableCollections.vue
+++ client/src/components/tables/TableCollections.vue
@@ -2,7 +2,7 @@
 import { ref, inject } from "vue";
 import { RouterLink } from "vue-router";
 import { ButtonDropdown, ButtonInput, ButtonMiscellenous } from "../buttonui";
-import { AddToCartIcon, ChevronDownIcon } from "../icons";
+import { AddToCartIcon, ChevronDownIcon, ArrowRightIcon } from "../icons";
 
 const { showMarketplaceCartBag } = inject<any>("provider");
 
@@ -61,8 +61,11 @@ const timeDurations = [
     icon: false,
   },
 ];
+const collectionContainerRef = ref<any>(null);
 const showAddToCartButton = ref<boolean>(false);
 const showTableNavigationButton = ref<boolean>(false);
+const scrollByVisibility = ref<number>(0);
+const scrollByVisibilityPercentage = ref<number>(0);
 const isActiveBlockChain = ref<string>("Ethereum");
 const isActiveTime = ref<string>("Day");
 const changeBlockChainNetwork = (blockChainNetwork: { id: string }) => {
@@ -73,11 +76,40 @@ const changeTimeDuration = (timeDuration: { id: string }) => {
   isActiveTime.value = timeDuration.id;
   console.log(`Time duration in ${isActiveTime.value} ago`);
 };
+const showAddToCartButtonIndex = ref<number>();
+const showAddToCartButtonCallback = (callBackIndex: number) => {
+  showAddToCartButtonIndex.value = callBackIndex;
+  return (showAddToCartButton.value = true);
+};
+const scrollBarCallback = (direction: string) => {
+  const { offsetWidth, scrollWidth, children } = collectionContainerRef.value;
+  const collectionItemWidth = children[0].offsetWidth;
 
-// const topHotNFTCOllection = [
+  const scrollByIndex =
+    direction == "forward" ? collectionItemWidth : -collectionItemWidth;
+  collectionContainerRef.value.scrollBy({
+    top: 0,
+    left: scrollByIndex,
+    behavior: "smooth",
+  });
+  scrollByVisibility.value += scrollByIndex;
 
-// ]
+  const progressiveScrollWidthTrackingLength =
+    offsetWidth + scrollByVisibility.value;
+
+  scrollByVisibilityPercentage.value =
+    (progressiveScrollWidthTrackingLength * 100) / scrollWidth;
+};
 </script>
+<style scoped>
+.hide-horizontal__scrollbar::-webkit-scrollbar {
+  display: none;
+}
+.hide-horizontal__scrollbar {
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
+}
+</style>
 <template>
   <div class="relative w-full py-10 text-gray-700 dark:text-darkTheme-text">
     <div class="flex flex-col">
@@ -156,24 +188,27 @@ const changeTimeDuration = (timeDuration: { id: string }) => {
               class="sf:w-full lmax:w-1/2 lmax:inline-flex sf:border-none lg:border-none border-solid border-transparent border-x-8 border-t-8"
             >
               <div
-                class="w-[99%] lmax:w-full lmax:border lmax:overflow-hidden lmax:dark:border-darkTheme-border flex lmax:flex-col flex-row justify-start lmax:p-5 sf:p-4 lmax:gap-5 py-1 pl-8 pr-2.5 rounded-2xl hover:bg-gray-50 dark:hover:bg-darkTheme-200 cursor-pointer lg:mx-auto my-1.5"
+                class="w-[99%] lmax:w-full lmax:border lmax:overflow-hidden lmax:dark:border-darkTheme-border flex lmax:flex-col flex-row justify-start lmax:p-5 sf:p-4 lmax:gap-5 py-1 pl-8 pr-2.5 rounded-2xl lg:hover:bg-gray-50 lg:dark:hover:bg-darkTheme-200 cursor-pointer lg:mx-auto my-1.5"
               >
                 <router-link
                   :to="{
                     path: '/collection/Art BLocks x Pace',
                   }"
-                  class="w-3/12 lmax:w-10/12 flex items-center dark:text-darkTheme-text-b"
+                  class="w-3/12 lmax:w-full flex lmax:justify-between items-center dark:text-darkTheme-text-b"
                 >
+                <div class="w-full sm:w-[90%] flex items-center gap-4">
                   <div
                     class="animate-pulse h-14 w-14 min-w-[56px] rounded-xl bg-gray-200 dark:bg-darkTheme-bg-0.08"
                     title="Contract address"
                   ></div>
                   <div
-                    class="text-lg font-semibold pr-2 pl-3 truncate"
+                    class="text-lg font-semibold truncate"
                     title="Art BLocks x Pace"
                   >
                     Art BLocks x Pace
                   </div>
+                </div>
+                  <ArrowRightIcon class="lg:hidden" />
                 </router-link>
                 <div
                   class="lg:hidden w-full flex justify-start text-sm text-gray-400 dark:text-darkTheme-text pt-1.5"
@@ -221,58 +256,61 @@ const changeTimeDuration = (timeDuration: { id: string }) => {
                     </div>
                   </div>
                 </router-link>
-                <div class="relative w-5/12 lmax:w-full overflow-hidden">
+                <div
+                  class="relative w-5/12 lmax:w-full overflow-hidden"
+                  v-on:mouseover="showTableNavigationButton = true"
+                  v-on:mouseout="showTableNavigationButton = false"
+                >
                   <div
-                    v-on:mouseover="showTableNavigationButton = true"
-                    v-on:mouseout="showTableNavigationButton = false"
-                    class="relative w-full h-auto flex flex-row space-x-4 items-center lmax:ml-1 lg:p-1"
+                    @click="scrollBarCallback('forward')"
+                    v-show="
+                      showTableNavigationButton &&
+                      scrollByVisibilityPercentage < 90
+                    "
+                    class="absolute h-full flex right-0 z-10 items-center"
                   >
-                    <div v-show="showTableNavigationButton" class="absolute h-full flex right-0 z-10 items-center">
-                      <span
-                        class="bg-white border border-white dark:border-darkTheme-border dark:bg-darkTheme rounded-2xl p-2 m-auto transition-all hover:scale-110 active:scale-100"
-                      >
-                        <chevron-down-icon class="rotate-[270deg]" />
-                      </span>
-                      <div
-                        class="bg-gradient-to-l from-white dark:from-darkTheme w-4 h-full"
-                      ></div>
-                    </div>
-                    <div v-show="showTableNavigationButton" class="absolute h-full flex left-0 z-10 items-center">
-                      <div
-                        class="bg-gradient-to-r from-white dark:from-darkTheme w-4 h-full"
-                      ></div>
-                      <span
-                        class="bg-white border border-white dark:border-darkTheme-border dark:bg-darkTheme rounded-2xl p-2 m-auto transition-all hover:scale-110 active:scale-100"
-                      >
-                        <chevron-down-icon class="rotate-[90deg]" />
-                      </span>
-                    </div>
-                    <div
-                      v-on:mouseover="showAddToCartButton = true"
-                      v-on:mouseout="showAddToCartButton = false"
-                      class="animate-pulse relative w-[100px] h-[100px] min-w-[100px] rounded-xl bg-gray-100 dark:bg-darkTheme-bg-0.08 hover:ring hover:ring-offset-0 hover:ring-gray-200 dark:hover:ring-darkTheme-200 hover:transition-all lmax:my-1"
+                    <span
+                      class="bg-white border border-white dark:border-darkTheme-border dark:bg-darkTheme rounded-2xl p-2 m-auto transition-all hover:scale-110 active:scale-100"
                     >
-                      <div
-                        v-show="showAddToCartButton"
-                        class="absolute border dark:border-darkTheme-border rounded-full bg-gray-200 dark:bg-darkTheme-bg p-1 right-0 m-1"
-                      >
-                        <add-to-cart-icon />
-                      </div>
-                      <div
-                        class="absolute bg-darkTheme-bgx rounded-xl w-[95%] mx-0.5 h-auto text-gray-100 text-xs p-1 bottom-1.5 whitespace-nowrap text-center space-x-1"
-                      >
-                        <span>ico</span>
-                        <span>0.104 ETH</span>
-                      </div>
-                    </div>
-
+                      <chevron-down-icon class="rotate-[270deg]" />
+                    </span>
                     <div
-                      v-on:mouseenter="showAddToCartButton = true"
+                      class="bg-gradient-to-l from-white dark:from-darkTheme w-4 h-full"
+                    ></div>
+                  </div>
+                  <div
+                    @click="scrollBarCallback('backward')"
+                    v-show="showTableNavigationButton && scrollByVisibility > 0"
+                    class="absolute h-full flex left-0 z-10 items-center"
+                  >
+                    <div
+                      class="bg-gradient-to-r from-white dark:from-darkTheme w-4 h-full"
+                    ></div>
+                    <span
+                      class="bg-white border border-white dark:border-darkTheme-border dark:bg-darkTheme rounded-2xl p-2 m-auto transition-all hover:scale-110 active:scale-100"
+                    >
+                      <chevron-down-icon class="rotate-[90deg]" />
+                    </span>
+                  </div>
+                  <div
+                    ref="collectionContainerRef"
+                    class="hide-horizontal__scrollbar relative w-full h-auto flex flex-row space-x-4 items-center lmax:ml-1 lg:p-1 overflow-x-auto snap-x snap-mandatory"
+                  >
+                    <div
+                      v-for="(datum, index) in [
+                        0.122, 2.234, 3, 0.44, 8, 1.11, 11, 0.11, 0.9,
+                      ]"
+                      :key="datum"
+                      :title="'Add NFT to your cart bag'"
+                      class="snap-start animate-pulse relative w-[100px] h-[100px] min-w-[100px] rounded-xl bg-gray-100 dark:bg-darkTheme-bg-0.08 hover:ring hover:ring-offset-0 hover:ring-gray-200 dark:hover:ring-darkTheme-200 hover:transition-all lmax:my-1"
+                      v-on:mouseenter="showAddToCartButtonCallback(index)"
                       v-on:mouseleave="showAddToCartButton = false"
-                      class="animate-pulse relative w-[100px] h-[100px] min-w-[100px] rounded-xl bg-gray-100 dark:bg-darkTheme-bg-0.08 hover:ring hover:ring-offset-0 hover:ring-gray-200 dark:hover:ring-darkTheme-200 hover:transition-all lmax:my-1"
                     >
                       <div
-                        v-show="showAddToCartButton"
+                        v-show="
+                          showAddToCartButton &&
+                          showAddToCartButtonIndex == index
+                        "
                         class="absolute border dark:border-darkTheme-border rounded-full bg-gray-200 dark:bg-darkTheme-bg p-1 right-0 m-1"
                       >
                         <add-to-cart-icon />
@@ -281,65 +319,18 @@ const changeTimeDuration = (timeDuration: { id: string }) => {
                         class="absolute bg-darkTheme-bgx rounded-xl w-[95%] mx-0.5 h-auto text-gray-100 text-xs p-1 bottom-1.5 whitespace-nowrap text-center space-x-1"
                       >
                         <span>ico</span>
-                        <span>0.104 ETH</span>
+                        <span>{{ datum }} ETH</span>
                       </div>
                     </div>
-
                     <div
-                      v-on:mouseenter="showAddToCartButton = true"
-                      v-on:mouseleave="showAddToCartButton = false"
-                      class="relative w-[100px] h-[100px] min-w-[100px] rounded-xl bg-gray-100 dark:bg-darkTheme-bg-0.08 hover:ring hover:ring-offset-0 hover:ring-gray-200 dark:hover:ring-darkTheme-200 hover:transition-all lmax:my-1"
+                      class="snap-start relative w-[100px] h-[100px] min-w-[100px] flex justify-center items-center lmax:my-1"
                     >
-                      <div
-                        v-show="showAddToCartButton"
-                        class="absolute border dark:border-darkTheme-border rounded-full bg-gray-200 dark:bg-darkTheme-bg p-1 right-0 m-1"
-                      >
-                        <add-to-cart-icon />
-                      </div>
-                      <div
-                        class="absolute bg-darkTheme-bgx rounded-xl w-[95%] mx-0.5 h-auto text-gray-100 text-xs p-1 bottom-1.5 whitespace-nowrap text-center space-x-1"
-                      >
-                        <span>ico</span>
-                        <span>0.104 ETH</span>
-                      </div>
-                    </div>
-
-                    <div
-                      v-on:mouseenter="showAddToCartButton = true"
-                      v-on:mouseleave="showAddToCartButton = false"
-                      class="relative w-[100px] h-[100px] min-w-[100px] rounded-xl bg-gray-100 dark:bg-darkTheme-bg-0.08 hover:ring hover:ring-offset-0 hover:ring-gray-200 dark:hover:ring-darkTheme-200 hover:transition-all lmax:my-1"
-                    >
-                      <div
-                        v-show="showAddToCartButton"
-                        class="absolute border dark:border-darkTheme-border rounded-full bg-gray-200 dark:bg-darkTheme-bg p-1 right-0 m-1"
-                      >
-                        <add-to-cart-icon />
-                      </div>
-                      <div
-                        class="absolute bg-darkTheme-bgx rounded-xl w-[95%] mx-0.5 h-auto text-gray-100 text-xs p-1 bottom-1.5 whitespace-nowrap text-center space-x-1"
-                      >
-                        <span>ico</span>
-                        <span>0.104 ETH</span>
-                      </div>
-                    </div>
-
-                    <div
-                      v-on:mouseenter="showAddToCartButton = true"
-                      v-on:mouseleave="showAddToCartButton = false"
-                      class="relative w-[100px] h-[100px] min-w-[100px] rounded-xl bg-gray-100 dark:bg-darkTheme-bg-0.08 hover:ring hover:ring-offset-0 hover:ring-gray-200 dark:hover:ring-darkTheme-200 hover:transition-all lmax:my-1"
-                    >
-                      <div
-                        v-show="showAddToCartButton"
-                        class="absolute border dark:border-darkTheme-border rounded-full bg-gray-200 dark:bg-darkTheme-bg p-1 right-0 m-1"
-                      >
-                        <add-to-cart-icon />
-                      </div>
-                      <div
-                        class="absolute bg-darkTheme-bgx rounded-xl w-[95%] mx-0.5 h-auto text-gray-100 text-xs p-1 bottom-1.5 whitespace-nowrap text-center space-x-1"
-                      >
-                        <span>ico</span>
-                        <span>0.104 ETH</span>
-                      </div>
+                      <router-link
+                        :to="{
+                          path: '/collection/Art BLocks x Pace',
+                        }"
+                        ><ArrowRightIcon
+                      /></router-link>
                     </div>
                   </div>
                 </div>
