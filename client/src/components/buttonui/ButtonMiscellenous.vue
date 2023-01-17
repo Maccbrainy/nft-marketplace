43d2bb84ee0a1d2f3f749ce09e80a2cec8581586
Maccbrainy <iketakumichaelnedum@gmail.com>
2023-01-17 16:56:00 +0100

    Minor: css update

--- client/src/components/buttonui/ButtonMiscellenous.vue
+++ client/src/components/buttonui/ButtonMiscellenous.vue
@@ -48,7 +48,7 @@ export default {
         'bg-gray-100 dark:bg-darkTheme-hover dark:text-darkTheme-text-b':
           openDropDownMenu && hasListContent,
       }"
-      class="relative h-full outline-none transition-all px-5 transform active:scale-95 whitespace-nowrap"
+      class="relative w-full h-full outline-none transition-all px-5 transform active:scale-95 whitespace-nowrap"
     >
       <div
         :class="{
