9967771313af9bc0f71fef13715a6b74e47f4536
Maccbrainy <iketakumichaelnedum@gmail.com>
2023-01-21 18:50:31 +0100

    Core: utility function for checking if a token/item is found in the marketplace cart at every point in time

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -1,6 +1,19 @@
 import type { MarketplaceCartBagItemType } from "@/types";
 
-import type { MarketplaceCartBagItemType } from "@/types";
-
 export const shortenAddress = (address: string) =>
   `${address.slice(0, 5)}...${address.slice(-4)}`;
+
+export const isMarketItemFoundInCartBagItems = (
+  token: {
+    tokenId: number;
+    tokenAddress: string;
+  },
+  marketplaceCartBagItems: MarketplaceCartBagItemType[]
+): boolean => {
+  const { tokenId, tokenAddress } = token;
+  const isTruthy: boolean = marketplaceCartBagItems.some(
+    (item: any) =>
+      item.token_address == tokenAddress && item.token_id == tokenId
+  );
+  return isTruthy;
+};
