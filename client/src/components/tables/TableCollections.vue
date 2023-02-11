54fe8cc9ecca7cfca8cf79f739fde1263574a543
Maccbrainy <iketakumichaelnedum@gmail.com>
2023-02-11 18:51:25 +0100

    Code clean up and css updates

--- client/src/components/tables/TableCollections.vue
+++ client/src/components/tables/TableCollections.vue
@@ -337,25 +337,19 @@ const scrollBarCallback = (direction: string) => {
                       showTableNavigationButton &&
                       scrollByVisibilityPercentage < 90
                     "
-                    class="absolute h-full flex right-0 z-10 items-center"
+                    class="absolute h-full flex right-3 z-10 items-center"
                   >
                     <span
                       class="bg-white border border-white dark:border-darkTheme-border dark:bg-darkTheme rounded-2xl p-2 m-auto transition-all hover:scale-110 active:scale-100"
                     >
                       <chevron-down-icon class="rotate-[270deg]" />
                     </span>
-                    <div
-                      class="bg-gradient-to-l from-white dark:from-darkTheme w-4 h-full"
-                    ></div>
                   </div>
                   <div
                     @click="scrollBarCallback('backward')"
                     v-show="showTableNavigationButton && scrollByVisibility > 0"
-                    class="absolute h-full flex left-0 z-10 items-center"
+                    class="absolute h-full flex left-3 z-10 items-center"
                   >
-                    <div
-                      class="bg-gradient-to-r from-white dark:from-darkTheme w-4 h-full"
-                    ></div>
                     <span
                       class="bg-white border border-white dark:border-darkTheme-border dark:bg-darkTheme rounded-2xl p-2 m-auto transition-all hover:scale-110 active:scale-100"
                     >
