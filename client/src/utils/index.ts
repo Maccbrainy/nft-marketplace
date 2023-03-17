2c070106fcfe2d3f95027ee46ce2a42f4f304ef9
Maccbrainy <iketakumichaelnedum@gmail.com>
2023-03-17 12:37:25 +0100

    Utility composable function useHandleImageChange FileReader function

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -1,7 +1,7 @@
 import type { MarketplaceCartBagItemType } from "@/types";
 
-export const shortenAddress = (address: string) =>
-  `${address.slice(0, 5)}...${address.slice(-4)}`;
+export const shortenAddress = (address: string, addressLength: number) =>
+  `${address.slice(0, addressLength)}...${address.slice(-4)}`;
 
 export const isMarketItemFoundInCartBagItems = (
   token: {
@@ -16,4 +16,22 @@ export const isMarketItemFoundInCartBagItems = (
       item.token_address == tokenAddress && item.token_id == tokenId
   );
   return isTruthy;
+};
+
+export const useHandleImageChange = async (event: {
+  target: { files: any[] };
+}) => {
+  const file = event.target.files[0];
+  let fileurl: string = "";
+  const reader = (readFile: Blob) =>
+    new Promise<string>((resolve, _reject) => {
+      const fileReader = new FileReader();
+      fileReader.onload = () => resolve(fileReader.result as string);
+      fileReader.readAsDataURL(readFile);
+    });
+  await reader(file).then((result: string) => (fileurl = result));
+  return {
+    fileurl,
+    fileName: file.name,
+  };
 };
