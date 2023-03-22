f4255d7536d9347665901ace2e67ccc7f795c17c
Maccbrainy <iketakumichaelnedum@gmail.com>
2023-03-22 21:18:11 +0100

    Fix: sidebar width at very large screens

--- client/src/layouts/SideBarLayout.vue
+++ client/src/layouts/SideBarLayout.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="relative w-full sm:w-[340px] 2xl:w-3/12 animate-slide-in-right bg-white dark:bg-darkTheme dark:text-white sm:m-5 sm:rounded-2xl shadow-2xl dark:shadow-2xl overflow-y-auto"
+    class="relative w-full sm:w-[340px] 2xl:w-[480px] animate-slide-in-right bg-white dark:bg-darkTheme dark:text-white sm:m-5 sm:rounded-2xl shadow-2xl dark:shadow-2xl overflow-y-auto"
   >
     <slot name="default"></slot>
   </div>
