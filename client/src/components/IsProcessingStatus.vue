c42492f973844aa4797c167d15836ea33e1abd2d
Maccbrainy <iketakumichaelnedum@gmail.com>
2023-03-22 21:16:13 +0100

    removed width and height class styles

--- client/src/components/IsProcessingStatus.vue
+++ client/src/components/IsProcessingStatus.vue
@@ -1,6 +1,6 @@
 <template>
   <svg
-    class="animate-spin h-5 w-5 text-gray-300"
+    class="animate-spin text-gray-300"
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 24"
