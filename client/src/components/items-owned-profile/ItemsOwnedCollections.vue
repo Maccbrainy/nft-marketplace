ad418e314dd7f67c6678f20a11e6990dd1c6ffcf
Maccbrainy <iketakumichaelnedum@gmail.com>
2023-03-22 21:02:05 +0100

    Initial commit

---
+++ client/src/components/items-owned-profile/ItemsOwnedCollections.vue
@@ -0,0 +1,3 @@
+<template>
+    <div>Import your collection</div>
+</template>
---
+++ client/src/components/items-owned-profile/index.ts
@@ -0,0 +1,1 @@
+export { default as ItemsOwnedCollections } from "@/components/items-owned-profile/ItemsOwnedCollections.vue";
